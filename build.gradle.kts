34d78e33c92c2a51b40e9794fcce13c4b9961d62
tkdgns8234 <tkdgns82345@naver.com>
2025-12-31 12:35:37 +0900

    mission-0001
    embedded kafka

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ description = "p-14652-1"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(25)
+        languageVersion = JavaLanguageVersion.of(17)
     }
 }
 
---
+++ src/test/java/com/back/infra/kafka/MyEvent.java
@@ -0,0 +1,7 @@
+package com.back.infra.kafka;
+
+import com.back.standard.event.HasEventName;
+
+public record MyEvent(String msg) implements HasEventName {
+
+}
